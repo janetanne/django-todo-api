b8310286ce996b71203d6e2751ae85eb0949d119
janet anne panen <janetpanen@gmail.com>
2021-03-23 21:11:45 -0700

    Create ToDo class for database in todo_api/models.py

--- todo_api/models.py
+++ todo_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class ToDo(models.Model):
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
